4ecb84d606c03c4afa94b388f8d2fb020393c012
Caelou <lucas.zimmer@epitech.eu>
2019-12-06 18:44:52 +0100

    [+] arrow game

--- Assets/Ressource/Arrow/arrow.cs
+++ Assets/Ressource/Arrow/arrow.cs
@@ -11,6 +11,8 @@ public class arrow : MonoBehaviour
     private Rigidbody2D rb;
     private SpringJoint2D sj;
     private Rigidbody2D slingRb;
+    public Camera cam;
+
     public GameObject bow;
 
     private float releaseDelay;
@@ -31,6 +33,18 @@ public class arrow : MonoBehaviour
         {
             DragBall();
         }
+        if (cam.WorldToViewportPoint(rb.position).x < 0 ||
+            cam.WorldToViewportPoint(rb.position).x > 1 &&
+            cam.WorldToViewportPoint(rb.position).y > 1 ||
+            cam.WorldToViewportPoint(rb.position).y < 0) {
+            bow.transform.eulerAngles = new Vector3(0, 0, 180);
+            transform.position = new Vector3((float)-7.7, 0, 0);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            sj.enabled = true;
+            rb.isKinematic = false;
+            rb.bodyType = RigidbodyType2D.Static;
+
+        }
     }
 
     private void DragBall()
@@ -44,11 +58,13 @@ public class arrow : MonoBehaviour
         } else {
             rb.position = mousePosition;
         }
-        float o = mousePosition.x - 0;
-        float a = mousePosition.y - -3;
-        double angle = 90 - Atan(o / a) * (180.0 / 3.141592653589793238463);
-        rb.rotation = (float)angle;
-        bow.transform.eulerAngles = new Vector3(0, 0, (float)angle + 180);
+        float o = mousePosition.x - (float)-7.7;
+        float a = mousePosition.y - 0;
+        double angle = 90 - Atan(o / a) * (180.0 / PI);
+       if (mousePosition.y < 0)
+            angle += 180;
+        rb.rotation = (float)angle + 180;
+        bow.transform.eulerAngles = new Vector3(0, 0, (float)angle);
     }
 
     private void OnMouseDown()
