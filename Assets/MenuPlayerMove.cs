7d1576e884ce313be96e8a263df0ff56a5751373
Jean Pignouf <ratouney1998@gmail.com>
2019-12-09 16:22:15 +0100

    Euhm, ca devrait marcher

---
+++ Assets/MenuPlayerMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPlayerMove : MonoBehaviour
+{
+    public Vector3 destination;
+    public float speed = 0.1f;
+
+    public bool moving = false;
+
+    GameObject playerModel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerModel = GameObject.Find("playerSprite");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving && transform.position != destination)
+        {
+            playerModel.GetComponent<Animator>().Play("Move");
+            Vector3 tmp = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+            transform.position = tmp;
+        }
+        if (moving && transform.position == destination)
+        {
+            playerModel.GetComponent<Animator>().Play("Idle");
+            moving = false;
+        }
+    }
+}
--- Assets/Script/PinController.cs
+++ Assets/Script/PinController.cs
@@ -14,16 +14,23 @@ public class PinController : MonoBehaviour
     [Range(1, 12)]
     public int MissionCount;
 
+    public float moveSpeed;
+
     PolygonCollider2D pinShape;
 
     GameObject MissionStatement;
+    GameObject player;
     Text missionTitle;
     Text missionBody;
+
+    MenuPlayerMove mpm;
 
     // Start is called before the first frame update
     void Start()
     {
         pinShape = GetComponent<PolygonCollider2D>();
+        player = GameObject.Find("Player");
+        mpm = player.GetComponent<MenuPlayerMove>();
         MissionStatement = GameObject.Find("MissionStatement");
         missionTitle = GameObject.Find("MissionTitle").GetComponent<Text>();
         //missionBody = GameObject.Find("MissionBody").GetComponent<Text>();
@@ -43,6 +50,7 @@ public class PinController : MonoBehaviour
 
             if (pinShape.bounds.Contains(mpos))
             {
+                player.transform.position = Vector3.MoveTowards(player.transform.position, transform.position, moveSpeed * Time.deltaTime);
                 if (MissionStatement.GetComponent<SlideAnimation>().show == false)
                 {
                     MissionStatement.GetComponent<SlideAnimation>().show = true;
@@ -51,6 +59,9 @@ public class PinController : MonoBehaviour
                 Debug.Log($"Setting title to {Title}");
                 MissionStatement.GetComponentInChildren<MissionNodes>().NodeCount = MissionCount;
                 Debug.Log($"Setting Nodecount to {MissionStatement.GetComponentInChildren<MissionNodes>().NodeCount}");
+                mpm.destination = new Vector3(transform.position.x - 0.6f, transform.position.y - 0.2f, transform.position.z);
+                mpm.moving = true;
+                Debug.Log("Move player to pin coordinates");
                 missionTitle.text = Title;
                 
             }
