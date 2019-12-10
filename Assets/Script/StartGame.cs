0260ba77a1c6c7de4869a1f421caa7c151f3dcd5
Afodia <bourgeay.ch@gmail.com>
2019-12-10 14:11:31 +0100

    shoot lion mini game done

---
+++ Assets/Script/StartGame.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour
+{
+    [SerializeField] Text startGameText = null;
+
+    void Start()
+    {
+        GameObject.Find("CountDownText").GetComponent<CountDownTimer>().enabled = false;
+        GameObject.Find("lion").GetComponent<LionMove>().enabled = false;
+        startGameText.text = "Lol";
+        print("lol");
+    }
+
+    void Update()
+    {
+        if (Input.touches[0].phase == TouchPhase.Began)
+        {
+            GameObject.Find("CountDownText").GetComponent<CountDownTimer>().enabled = true;
+        }
+    }
+
+    public void Win()
+    {
+        GameObject.Find("CountDownText").GetComponent<CountDownTimer>().enabled = false;
+        startGameText.text = "Mes flèches lui rebondissent dessus, en plus il s'est enfui.";
+    }
+
+    public void Loose()
+    {
+        GameObject.Find("CountDownText").GetComponent<CountDownTimer>().enabled = false;
+        startGameText.text = "Il faut que je réessaye.";
+    }
+}
---
+++ Assets/Script/StartMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = true;
+    public static bool GameIsEnded = false;
+    public GameObject PauseMenuUI;
+    [SerializeField] Text displayText = null;
+
+    void Start()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    void Update()
+    {
+    }
+
+    void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        GameIsPaused = false;
+        Time.timeScale = 1f;
+    }
+
+    public void End()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Win()
+    {
+        PauseMenuUI.SetActive(true);
+        displayText.text = "Mes flèches lui rebondissent dessus, en plus il s'est enfui.\n\nIl va falloir trouver autre chose.";
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        GameIsEnded = true;
+    }
+}
