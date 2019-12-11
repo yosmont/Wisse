fb06765c3cf54bd7b2dda5b2fca8e5cb6f3d5387
Caelou <lucas.zimmer@epitech.eu>
2019-12-11 14:35:10 +0100

    [+] battle minigame

--- Assets/BattleSystem.cs
+++ Assets/BattleSystem.cs
@@ -10,6 +10,8 @@ public class BattleSystem : MonoBehaviour
     public BattleState state;
     public GameObject player;
     public GameObject enemy;
+    private GameObject playerGo;
+    private GameObject enemyGo;
 
     public Transform playerBattleStation;
     public Transform enemyBattleStation;
@@ -17,9 +19,10 @@ public class BattleSystem : MonoBehaviour
     public BattleHUD playerHUD;
     public BattleHUD enemyHUD;
 
-
     public Text DialogueText;
 
+    private GameObject Button;
+    private bool sword = true;
     Unit playerUnit;
     Unit enemyUnit;
 
@@ -34,10 +37,12 @@ public class BattleSystem : MonoBehaviour
 
     IEnumerator SetupBattle()
     {
-        GameObject playerGo = Instantiate(player, playerBattleStation);
+        Button = GameObject.Find("Etranglement");
+        Button.SetActive(false);
+        playerGo = Instantiate(player, playerBattleStation);
         playerUnit = playerGo.GetComponent<Unit>();
 
-        GameObject enemyGo = Instantiate(enemy, enemyBattleStation);
+        enemyGo = Instantiate(enemy, enemyBattleStation);
         enemyUnit = enemyGo.GetComponent<Unit>();
 
         DialogueText.text = enemyUnit.unitName + " apparait !";
@@ -50,13 +55,27 @@ public class BattleSystem : MonoBehaviour
         PlayerTurn();
     }
 
-    IEnumerator PlayerAttack()
+    IEnumerator PlayerAttack(string type, int dmg)
     {
-        bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+        bool isDead = enemyUnit.TakeDamage(dmg);
 
+        playerGo.GetComponent<Animator>().Play("Hit");
+        yield return new WaitForSeconds(1f);
         enemyHUD.setHP(enemyUnit.currentHp);
         DialogueText.text = "Touché !";
         yield return new WaitForSeconds(2f);
+
+        if (type == "sword")
+        {
+            DialogueText.text = "Ton épée s'est brisée contre le corps du lion";
+            
+            yield return new WaitForSeconds(2f);
+        }
+        if (type == "mace")
+        {
+            DialogueText.text = "Le bruit dérange " + enemyUnit.unitName;
+            yield return new WaitForSeconds(2f);
+        }
         if (isDead)
         {
             state = BattleState.WIN;
@@ -80,10 +99,12 @@ public class BattleSystem : MonoBehaviour
 
     IEnumerator EnemyTurn()
     {
-        DialogueText.text = enemyUnit.unitName + "attaque !";
-        yield return new WaitForSeconds(2f);
+        DialogueText.text = enemyUnit.unitName + " attaque !";
+        enemyGo.GetComponent<Animator>().Play("Hit");
+        yield return new WaitForSeconds(1f);
         bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
         playerHUD.setHP(playerUnit.currentHp);
+        yield return new WaitForSeconds(2f);
         if (isDead)
         {
             state = BattleState.LOSE;
@@ -97,14 +118,37 @@ public class BattleSystem : MonoBehaviour
 
     void PlayerTurn()
     {
+        if (sword == false)
+            Button.SetActive(true);
         DialogueText.text = "À ton tour !";
+    }
+
+    public void OnMaceButton()
+    {
+        if (state != BattleState.PLAYERTURNE)
+            return;
+        StartCoroutine(PlayerAttack("mace", 1));
+    }
+
+    public void OnThrottlingButton()
+    {
+        if (state != BattleState.PLAYERTURNE)
+            return;
+        StartCoroutine(PlayerAttack("throttling", 100));
     }
 
     public void OnSwordButton()
     {
-        if (state != BattleState.PLAYERTURNE)
-            return;
-        StartCoroutine(PlayerAttack());
+        if (sword)
+        {
+            if (state != BattleState.PLAYERTURNE)
+                return;
+            sword = false;
+            StartCoroutine(PlayerAttack("sword", 1));
+        }
+        else {
+            DialogueText.text = "Ton épée est cassée";
+        }
     }
 
 
