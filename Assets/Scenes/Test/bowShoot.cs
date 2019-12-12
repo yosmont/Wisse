dc6c865f183413d38da79432aee3d9641a190089
yosmont <yann.osmont@epitech.eu>
2019-12-12 16:27:32 +0100

    Cave NavMesh

---
+++ Assets/Scenes/Test/bowShoot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bowShoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 InputWorldPoint = Vector3.zero;
+        if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.Lumin || Application.platform == RuntimePlatform.IPhonePlayer) {
+            if (Input.touchCount > 0) {
+                if (Input.touches[0].phase != TouchPhase.Ended && Input.touches[0].phase != TouchPhase.Canceled) {
+                    InputWorldPoint = Camera.main.ScreenToWorldPoint(Input.touches[0].position);
+                }
+            }
+        } else {
+            if (Input.GetMouseButton(0)) {
+                InputWorldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+        }
+        if (InputWorldPoint == Vector3.zero) {
+            RaycastHit2D hit = Physics2D.Raycast(new Vector2(InputWorldPoint.x, InputWorldPoint.y), Vector2.zero);
+            if (hit.collider != null && hit.collider.name == "Bow&Arrow") {
+
+            }
+        }
+    }
+}
