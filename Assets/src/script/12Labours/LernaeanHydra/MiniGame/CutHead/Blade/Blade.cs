574938fe53b46a7b551274c9046001d15c0ce882
yosmont <yann.osmont@epitech.eu>
2020-04-01 15:12:56 +0200

    Lernaean Hydra - Cut Head Mini-Game - Blade (it's not a Runner)

---
+++ Assets/src/script/12Labours/LernaeanHydra/MiniGame/CutHead/Blade/Blade.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+    public GameObject _bladeTrailPrefab;
+    public float _minCuttingVelocity = 0.001f;
+    private Vector2 _prevPos;
+    private GameObject _currentBladeTrail;
+    private CircleCollider2D _circleColl;
+    private bool _isCutting = false;
+    private Camera _cam;
+    private Rigidbody2D _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _circleColl = GetComponent<CircleCollider2D>();
+        _circleColl.enabled = false;
+        _cam = Camera.main;
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            StartCutting();
+        } else if (Input.GetMouseButtonUp(0)) {
+            StopCutting();
+        }
+        if (_isCutting) {
+            UpdateCut();
+        }
+    }
+
+    void StartCutting()
+    {
+        _isCutting = true;
+        _rb.position = _cam.ScreenToWorldPoint(Input.mousePosition);
+        _prevPos = _rb.position;
+        _currentBladeTrail = Instantiate(_bladeTrailPrefab, _rb.transform);
+    }
+
+    void StopCutting()
+    {
+        _isCutting = false;
+        _circleColl.enabled = false;
+        _currentBladeTrail.transform.SetParent(null);
+        Destroy(_currentBladeTrail, 1f);
+    }
+
+    void UpdateCut()
+    {
+        _rb.position = _cam.ScreenToWorldPoint(Input.mousePosition);
+        if (((_rb.position - _prevPos).magnitude * Time.deltaTime) > _minCuttingVelocity) {
+            _circleColl.enabled = true;
+        } else {
+            _circleColl.enabled = false;
+        }
+        _prevPos = _rb.position;
+    }
+}
